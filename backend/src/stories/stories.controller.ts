c09bec2404807035cd4b9bd42364b115753e9f97
Sarah O. <sarah.shofani@proton.me>
2025-11-22 16:30:51 -0300

    feat: add route to get all stories

--- backend/src/stories/stories.controller.ts
+++ backend/src/stories/stories.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
+
 import { StoriesService } from './stories.service';
+import { Story as StoryModel } from '@prisma/client';
+import { GetStoriesDto } from './dtos/get-stories.dto';
 
 @Controller('story')
 export class StoriesController {
   constructor(private readonly storiesService: StoriesService) {}
 
+  @Get()
+  async getStories(@Query() query: GetStoriesDto): Promise<StoryModel[]> {
+    return await this.storiesService.getAllStories({
+      page: query.page,
+      limit: query.limit,
+      orderBy: query.orderBy
+        ? { [query.orderBy]: 'desc' }
+        : { createdAt: 'desc' },
+    });
+  }
+
   @Get(':params')
-  async getStory(@Param('params') params: string) {
+  async getStory(@Param('params') params: string): Promise<StoryModel> {
     const storyId = params.includes('-')
       ? parseInt(params.split('-')[0], 10)
       : parseInt(params, 10);
