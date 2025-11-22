96b62aaa8c696ba704141c1a1ec85e33873ea645
Sarah O. <sarah.shofani@proton.me>
2025-11-22 16:31:02 -0300

    feat: function to get all stories

--- backend/src/stories/stories.service.ts
+++ backend/src/stories/stories.service.ts
@@ -7,6 +7,19 @@ import { Story, Prisma } from '@prisma/client';
 export class StoriesService {
   constructor(private prisma: PrismaService) {}
 
+  async getAllStories(params: {
+    page: number;
+    limit: number;
+    orderBy?: Prisma.StoryOrderByWithRelationInput;
+  }): Promise<Story[]> {
+    const { page, limit, orderBy } = params;
+    return this.prisma.story.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+      orderBy,
+    });
+  }
+
   async getStoryById(id: number): Promise<Story | null> {
     return this.prisma.story.findUnique({ where: { id } });
   }
