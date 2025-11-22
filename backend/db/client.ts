70b3c8b62f0e7075a88623bc2bf72490be940949
Sarah O. <sarah.shofani@proton.me>
2025-11-22 17:11:52 -0300

    chore: export prisma const

--- backend/db/client.ts
+++ backend/db/client.ts
@@ -5,4 +5,4 @@ import { PrismaClient } from '@prisma/client';
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter });
