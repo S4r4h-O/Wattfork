4bce33e78815790e1d67d1d306450cdb0684a0b4
Sarah O. <sarah.shofani@proton.me>
2025-11-22 15:48:17 -0300

    fix: prisma now imported from @prisma/client, added to providers instead of imports

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [PrismaService],
   providers: [UsersService],
   controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
